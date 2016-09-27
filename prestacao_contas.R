766abe6a13b7e012a151b5d87c7ddcda938ab501
gpiraja <germanopiraja@gmail.com>
2016-09-26 23:47:06 -0300

    Extração de dados do arquivo compactado e dados no ws do R

--- prestacao_contas.R
+++ prestacao_contas.R
@@ -1,11 +1,20 @@
 # Automatização da obtenção dos dados do repositório do TSE
+## Baixando e extraindo dados do TSE
+### Dados da prestacao de contas parcial
+loc.url <- "http://agencia.tse.jus.br/estatistica/sead/odsele/prestacao_contas/prestacao_contas_parcial_2016.zip"
+td <- tempdir()
+tf <- tempfile(tmpdir=td, fileext=".zip")
+download.file(loc.url, tf)
+#### Relaciona o nome dos arquivos compactados
+lista_nome <- unzip(tf, list=TRUE)$Name
+#### Seleciona os arquivos relacionados ao Estado de SC
+lista_sc <- grep("SC", lista_nome)
 
-temp <- tempfile()
-download.file("http://agencia.tse.jus.br/estatistica/sead/odsele/prestacao_contas/cnpj_campanha_2016.zip"
-              ,temp)
-data <- read.csv(unz("prestacao_contas.zip", "despesas_candidatos_2016_SC.txt"))
-unlink(temp)
-unzip("")
-rec_cand <- read.csv("receitas_candidatos_2016_SC.txt", fileEncoding = "latin1", sep = ";", 
-                     colClasses = c(rep("character", 35)))
-rec_cand$Valor.receita <- as.numeric(gsub("[,]", ".", rec_cand$Valor.receita))
+#### Descompacta arquivos de interesse
+length(lista_sc)
+unzip(tf, files=lista_nome[lista_sc], exdir=td, overwrite=TRUE)
+for (i in lista_nome[lista_sc]) {
+        fpath <- file.path(td, i)
+        assign(i, read.csv(fpath, sep=";", fileEncoding = "latin1"))}
+
+
